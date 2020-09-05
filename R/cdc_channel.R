97f053924065affa1b73be6049246ed755e16280
avallecam <avallecam@gmail.com>
2020-09-04 22:57:32 -0500

    updated function + reprex workflow

--- R/cdc_channel.R
+++ R/cdc_channel.R
@@ -1,13 +1,16 @@
-#' @title Crear canal endémico
+#' @title Create an Endemic Channel
 #'
-#' @description Generar estimaciones y detección de brotes.
+#' @description Exploratory Alarm Tool for Outbreak Detection.
 #'
-#' @describeIn cdc_endemic_channel_mutate
+#' @describeIn epi_adapt_timeserie
 #'
-#' @param disease disease surveillance datasets
-#' @param population estimated population for each adm area
-#' @param name free code name of diseases
-#' @param method specify the method. "gmean_1sd" is geometric mean w/ 1 standard deviation (default). "gmean_2sd" is gmean w/ 2 sd. "gmean_ci" is gmean w/ 95 percent confidence intervals.
+#' @param db_disease disease surveillance datasets
+#' @param db_population estimated population for each adm area
+#' @param var_admx administrative code r name as string
+#' @param var_year year of agregated observations
+#' @param var_week week of agregated observations
+#' @param var_event_count number of events per week-year
+#' @param var_population estimated population at that year
 #'
 #' @import dplyr
 #' @import tidyr
@@ -16,39 +19,148 @@
 #'
 #' @return canal endemico, union y grafico
 #'
-#' @export cdc_endemic_channel_mutate
-#' @export cdc_endemic_channel_join
-#' @export cdc_endemic_channel_ggplot
+#' @export epi_adapt_timeserie
+#' @export epi_create_channel
+#' @export epi_join_channel
+#' @export epi_plot_channel
 #'
 #' @examples
 #'
-#' # not yet
+#' library(tidyverse)
 #'
-cdc_endemic_channel_mutate <- function(disease,population,name,method="gmean_1sd") {
+#' # import data -------------------------------------------------------------
+#'
+#' dengv <-
+#'   readr::read_csv("https://dengueforecasting.noaa.gov/Training/Iquitos_Training_Data.csv") %>%
+#'   mutate(year = lubridate::year(week_start_date),
+#'          epiweek = lubridate::epiweek(week_start_date)) %>%
+#'   mutate(adm="iquitos") %>%
+#'   # cases per season - replace wiht a dummy year
+#'   mutate(year = str_replace(season,"(.+)/(.+)","\\1") %>% as.double())
+#'
+#' # dengv %>% count(year,season,lag_year)
+#'
+#' dengv %>% glimpse()
+#'
+#' # dengv %>%
+#' #   ggplot(aes(x = week_start_date,y = total_cases)) +
+#' #   geom_col()
+#'
+#' popdb <-
+#'   readr::read_csv("https://dengueforecasting.noaa.gov/PopulationData/Iquitos_Population_Data.csv") %>%
+#'   janitor::clean_names() %>%
+#'   mutate(adm="iquitos")
+#'
+#' popdb %>% glimpse()
+#'
+#' # popdb %>% count(year)
+#' # dengv %>% count(year)
+#' # dengv %>% left_join(popdb)
+#'
+#' # first, adapt ------------------------------------------------------------
+#'
+#' epi_adapted <-
+#'   epi_adapt_timeserie(db_disease = dengv,
+#'                       db_population = popdb,
+#'                       var_admx = adm,
+#'                       # var_year = year, # must be a common variable between datasets
+#'                       # var_week = epiweek,
+#'                       var_year = year, # not working - need to create pseudo-years
+#'                       var_week = season_week,
+#'                       var_event_count = total_cases,
+#'                       var_population = estimated_population)
+#'
+#' # second, filter ----------------------------------------------------------
+#'
+#' disease_now <- epi_adapted %>%
+#'   filter(var_year==max(var_year))
+#'
+#' disease_pre <- epi_adapted %>%
+#'   filter(var_year!=max(var_year))
+#'
+#' # third, create -----------------------------------------------------------
+#'
+#' disease_channel <-
+#'   epi_create_channel(time_serie = disease_pre,
+#'                      disease_name = "dengv")
+#'
+#' disease_channel
+#'
+#' # fourth, ggplot it -------------------------------------------------------
+#'
+#' epi_join_channel(disease_channel = disease_channel,
+#'                  disease_now = disease_now) %>%
+#'   # ggplot
+#'   epi_plot_channel() +
+#'   labs(title = "DENGV Endemic Channel. Iquitos, Peru 2008/2009",
+#'        caption = "Source: https://dengueforecasting.noaa.gov/",
+#'        # x = "epiweeks",
+#'        x = "Seasonal week",
+#'        y = "Number of cases") +
+#'   theme_bw()
+#'
+#'
 
-  edapr_dis <- disease
-  pob_long <- population
+epi_adapt_timeserie <- function(db_disease,
+                                db_population,
+                                var_admx,
+                                var_year,
+                                var_week,
+                                var_event_count,
+                                var_population) {
+  db_disease_f <- db_disease %>%
+    #adapt variables
+    rename(var_admx={{var_admx}},
+           var_year={{var_year}},
+           var_week={{var_week}},
+           var_event_count={{var_event_count}})
+  db_population_f <- db_population %>%
+    #adapt variables
+    rename(var_population={{var_population}},
+           var_admx={{var_admx}},
+           var_year={{var_year}})
 
   parte_1_4 <-
-    #Paso 1:  N° de casos  por semana,  7 años
-    edapr_dis %>%
-    complete(ubigeo,
-             semana=full_seq(semana,1),
-             year=full_seq(year,1),fill = list(eda=0)) %>%
-    #Paso 1.2: unir con el tamaño poblacional por ubigeo-anho
-    left_join(pob_long) %>%
+    #Paso 1:  N° de casos  por var_week,  7 años
+    db_disease_f %>%
+    # complete missings
+    complete(var_admx,
+             var_week=full_seq(var_week,1),
+             var_year=full_seq(var_year,1),
+             fill = list(var_event_count=0)) %>%
+    #Paso 1.2: unir con el tamaño poplacional por var_admx-anho
+    left_join(db_population_f) %>%
 
-    ##Paso 1.3: retirar ubigeos sin poblacion para ningun anho
+    ##Paso 1.3: retirar ubigeos sin poplacion para ningun anho
     ##naniar::miss_var_summary()
-    ##filter(is.na(pob)) %>% count(ubigeo,year) %>% count(n)
-    #filter(!is.na(pob)) %>%
+    ##filter(is.na(var_population)) %>% count(var_admx,var_year) %>% count(n)
+    #filter(!is.na(var_population)) %>%
 
     #Paso 2: Cálculo de tasas y suma 1 (facilitar transformación en 0 casos)
-    mutate(tasa=eda/pob*100000+1,
+    mutate(tasa=var_event_count/var_population*100000+1,
            #Paso 3: Transformación logarítmica de las tasas
-           log_tasa=log(tasa)) %>%
+           log_tasa=log(tasa))
+
+  return(parte_1_4)
+}
+
+#' @describeIn epi_adapt_timeserie create endemic channel
+#' @inheritParams epi_adapt_timeserie
+#' @param time_serie time serie
+#' @param disease_name free code name of diseases
+#' @param method specify the method. "gmean_1sd" is geometric mean w/ 1 standard deviation (default). "gmean_2sd" is gmean w/ 2 sd. "gmean_ci" is gmean w/ 95 percent confidence intervals.
+
+epi_create_channel <- function(time_serie,
+                               disease_name="disease_name",
+                               method="gmean_1sd") {
+
+  parte_1_4 <- time_serie %>%
     #Paso 3: agrupar
-    group_by(ubigeo, semana)
+    group_by(var_admx, var_week)
+
+  db_population_f <- time_serie %>%
+    select(var_admx,var_year,var_population) %>%
+    distinct()
 
   if(method=="gmean_1sd"){
     parte_final <-
@@ -69,7 +181,7 @@ cdc_endemic_channel_mutate <- function(disease,population,name,method="gmean_1sd
   }
 
   if(method=="gmean_ci"){
-    # Paso 4: Cálculo de media e IC95% de log_tasas
+    # Paso 4: Cálculo de median e IC95% de log_tasas
     parte_final <-
       parte_1_4 %>%
       nest() %>%
@@ -77,10 +189,10 @@ cdc_endemic_channel_mutate <- function(disease,population,name,method="gmean_1sd
       # #ISSUE tagged -> non-vairability stops t.test
       # count(log_tasa,sort = T) %>%
       # ungroup() %>% filter(n==7) %>%
-      # count(ubigeo,sort = T)
+      # count(var_admx,sort = T)
       # count(log_tasa)
-      # filter(is.na(log_tasa)) %>% count(ubigeo)
-      # #SOLUTION: retirar semana con puro cero o NA
+      # filter(is.na(log_tasa)) %>% count(var_admx)
+      # #SOLUTION: retirar var_week con puro cero o NA
       # #PLAN 01
       mutate(sum_tasa=map_dbl(.x = data,.f = ~sum(.$log_tasa),na.rm=TRUE)) %>% #arrange(sum_tasa)
       filter(sum_tasa>0) %>%
@@ -90,13 +202,13 @@ cdc_endemic_channel_mutate <- function(disease,population,name,method="gmean_1sd
       # NOT WORKED: non-vairability  maintained after filter of values higher than 0
       #PLAN 02
       mutate(t_test=map(.x = data,.f = ~lm(log_tasa ~ 1,data=.x))) %>%
-        mutate(tidied=map(t_test,broom::tidy),
-               tidy_ci=map(t_test,broom::confint_tidy)) %>%
-        unnest(cols = c(tidied,tidy_ci)) %>%
-        rename(media_l=estimate,
-               lo_95_l=conf.low,
-               hi_95_l=conf.high) %>%
-        ungroup()
+      mutate(tidied=map(t_test,broom::tidy),
+             tidy_ci=map(t_test,broom::confint_tidy)) %>%
+      unnest(cols = c(tidied,tidy_ci)) %>%
+      rename(media_l=estimate,
+             lo_95_l=conf.low,
+             hi_95_l=conf.high) %>%
+      ungroup()
   }
 
 
@@ -105,55 +217,65 @@ cdc_endemic_channel_mutate <- function(disease,population,name,method="gmean_1sd
     mutate(media_t= exp(media_l)-1, #sd_t= exp(sd_l)-1,
            lo_95_t=exp(lo_95_l)-1, hi_95_t=exp(hi_95_l)-1) %>%
     # Paso 6: Transformación a de tasas a casos (esperados)
-    left_join(pob_long %>%
-                group_by(ubigeo) %>%
-                summarise(media_pob=mean(pob))%>%
+    left_join(db_population_f %>%
+                group_by(var_admx) %>%
+                summarise(media_pop=mean(var_population))%>%
                 ungroup()) %>%
-    mutate(media=media_t*media_pob/100000,
-           lo_95=lo_95_t*media_pob/100000,
-           hi_95=hi_95_t*media_pob/100000) %>%
+    mutate(median=media_t*media_pop/100000,
+           low_95=lo_95_t*media_pop/100000,
+           upp_95=hi_95_t*media_pop/100000) %>%
     # seleccionado solo las variables a usar
-    select(ubigeo, semana, media, lo_95, hi_95) %>%
-    mutate(key=name)%>%
-    mutate(ubigeo=as.factor(ubigeo)) %>%
+    select(var_admx, var_week, median, low_95, upp_95) %>%
+    mutate(key={{disease_name}})%>%
+    mutate(var_admx=as.factor(var_admx)) %>%
     return()
 
 }
 
-#' @describeIn cdc_endemic_channel_mutate unir resultado con vigilancia actual
-#' @inheritParams cdc_endemic_channel_mutate
-#' @param disease_channel salida de cdc_*_mutate
+#' @describeIn epi_adapt_timeserie join preliminary steps
+#' @inheritParams epi_adapt_timeserie
+#' @param disease_channel salida de epi_*_mutate
 #' @param disease_now nueva base de vigilancia
+#' @param adm_columns dataframe with names of the administrative code or name strings
 
-cdc_endemic_channel_join <- function(disease_channel,disease_now) {
+epi_join_channel <- function(disease_channel,
+                             disease_now,
+                             adm_columns=NULL) {
   #unir con canal
-  full_join(disease_channel,disease_now) %>%
-    left_join(
-      ubigeo %>% select(ubigeo,dist:dpto)
-    ) %>%
-    mutate_at(.vars = vars(value,lo_95,hi_95,media),replace_na,0)
+  out <- disease_channel %>%
+    full_join(disease_now)
+  if (!is.null(adm_columns)) {
+    out <- out %>%
+      left_join(
+        adm_columns
+      ) #%>%
+    # mutate_at(.vars = vars(value,low_95,upp_95,median),replace_na,0)
+  }
+  return(out)
 }
 
-#' @describeIn cdc_endemic_channel_mutate crear gráfico
-#' @inheritParams cdc_endemic_channel_mutate
+#' @describeIn epi_adapt_timeserie create ggplot
+#' @inheritParams epi_adapt_timeserie
 #' @param joined_channel base de datos unida
+#' @param n_breaks number of breaks in x axis
 
-cdc_endemic_channel_ggplot <- function(joined_channel) {
+epi_plot_channel <- function(joined_channel,n_breaks=10) {
   #unir con canal
   # plot_name <- joined_channel %>%
   #   select(dist,prov,dpto) %>%
   #   distinct() %>% as.character() %>% paste(collapse = ", ")
   joined_channel %>%
-    mutate(new= max(c(value,hi_95),na.rm = T),
+    mutate(new= max(c(var_event_count,upp_95),na.rm = T),
            #plot_name= str_c(dist,"\n",prov,"\n",dpto)
     ) %>%
-    ggplot(aes(x = semana, y = value#, fill=key
-               )) +
-    geom_area(aes(x=semana, y=new), fill = "#981000", alpha=0.6, stat="identity")+
-    geom_area(aes(x=semana, y=hi_95), fill = "#fee76a", stat="identity")+
-    geom_area(aes(x=semana, y=media), fill = "#3e9e39",  stat="identity")+
-    geom_area(aes(x=semana, y=lo_95), fill = "white", stat="identity") +
+    ggplot(aes(x = var_week, y = var_event_count#, fill=key
+    )) +
+    geom_area(aes(x=var_week, y=new), fill = "#981000", alpha=0.6, stat="identity")+
+    geom_area(aes(x=var_week, y=upp_95), fill = "#fee76a", stat="identity")+
+    geom_area(aes(x=var_week, y=median), fill = "#3e9e39",  stat="identity")+
+    geom_area(aes(x=var_week, y=low_95), fill = "white", stat="identity") +
     geom_line(size = 0.8) +
-    xlab("semanas") + ylab("N° de casos") #+
+    scale_x_continuous(breaks = scales::pretty_breaks(n = {{n_breaks}})) #+
+  # xlab("semanas") + ylab("N° de casos") #+
   #labs(title = paste0("Distrito de ",plot_name))
 }
